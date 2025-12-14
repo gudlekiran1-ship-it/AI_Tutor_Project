f459ced53e58bf25decf99edf90b461f838851c3
gudlekiran1-ship-it <gudlekiran1@gmail.com>
2025-12-14 22:21:24 +0530

    Add login function using Streamlit

--- auth.py
+++ auth.py
@@ -1,1 +1,13 @@
 
+import streamlit as st
+
+def login():
+    st.subheader("Login")
+    user = st.text_input("Username")
+    pwd = st.text_input("Password", type="password")
+
+    if st.button("Login"):
+        if user == "admin" and pwd == "admin":
+            st.session_state["login"] = True
+        else:
+            st.error("Invalid Credentials")
