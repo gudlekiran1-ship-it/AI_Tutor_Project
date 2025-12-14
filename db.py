5564fb80355fcdff37631ac342e6b57db64e0d7a
gudlekiran1-ship-it <gudlekiran1@gmail.com>
2025-12-14 22:31:33 +0530

    Switch database connection from MySQL to SQLite

--- db.py
+++ db.py
@@ -1,9 +1,5 @@
-import mysql.connector
+import sqlite3
 
 def get_db():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="aitutor"
-    )
+    conn = sqlite3.connect("users.db", check_same_thread=False)
+    return conn
