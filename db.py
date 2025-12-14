2a977184b80ca52e9960e4188a5ff144f4043203
gudlekiran1-ship-it <gudlekiran1@gmail.com>
2025-12-14 22:20:58 +0530

    Add MySQL and MongoDB connection functions

--- db.py
+++ db.py
@@ -1,1 +1,13 @@
+import mysql.connector
+from pymongo import MongoClient
 
+def mysql_conn():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="aitutor"
+    )
+
+def mongo_conn():
+    return MongoClient("mongodb://localhost:27017/")["aitutor"]
